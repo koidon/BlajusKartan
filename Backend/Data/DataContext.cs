d07adbc357a1aef0adb81748f359d924ff4b4769
koidon <sormesunda01@gmail.com>
2023-12-05 22:48:05 +0100

    Added initial database + Entity framework. Set InvariantGlobalization to false. Added basic endpoint.

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+
+
+   public DbSet<Book> Books => Set<Book>();
+
+}
---
+++ Backend/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models;
+
+public class Book
+{
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+}
