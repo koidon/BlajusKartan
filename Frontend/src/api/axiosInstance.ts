3bb708ae8f902a03a4db01191d4f5646a819f4a2
koidon <sormesunda01@gmail.com>
2023-12-11 23:41:57 +0100

    Changed baseURl to api hosted on azure

--- Frontend/src/api/axiosInstance.ts
+++ Frontend/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5118",
+  //baseURL: "http://localhost:5118",
+  baseURL: "blaljuskartan-api.azurewebsites.net",
   headers: {
     "Content-Type": "application/json",
   },
